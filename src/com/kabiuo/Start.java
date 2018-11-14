5a90ea27a70f21514db7a155526c99c5277254c9
kabiuo <852119338@qq.com>
2018-11-14 11:55:17 +0800

    Add files via upload

--- src/com/kabiuo/Start.java
+++ src/com/kabiuo/Start.java
@@ -7,87 +7,78 @@ import java.awt.BorderLayout;
 
 //import com.kabiuo.ThunderPanel;
 
-public class Start implements ActionListener{
-	private JFrame startJFrame = new JFrame("Mine Clearance");
+public class Start implements ActionListener {
+    private JFrame startJFrame = new JFrame("Mine Clearance");
+    private int lww = -1;
 //	private JPanel jPanelBoderlayoutCenter = new JPanel();
-	
-	Start(){
-		startJFrame.setSize(500,300);
-		startJFrame.setDefaultCloseOperation(3);
-		startJFrame.setLocationRelativeTo(null);
-		startJFrame.setLayout(new BorderLayout());
+
+    Start() {
+        startJFrame.setSize(500, 300);
+        startJFrame.setDefaultCloseOperation(3);
+        startJFrame.setLocationRelativeTo(null);
+        startJFrame.setLayout(new BorderLayout());
 //		jPanelBoderlayoutCenter.setLayout(new BorderLayout());
 //		jPanelBoderlayoutCenter.setBackground(Color.red);
 //		startJFrame.add(jPanelBoderlayoutCenter, BorderLayout.CENTER);
-		CreatMenuBar();
-		startJFrame.setVisible(true);
-	}
-	
-	public void CreatMenuBar(){
-		JMenuBar gameMenuBar = new JMenuBar();
-		JMenu gameMenu = new JMenu("Option");
-		JMenuItem gameItemSimple = new JMenuItem("Simple");
-		JMenuItem gameItemEasy = new JMenuItem("Easy");
-		JMenuItem gameItemDifficulty = new JMenuItem("Difficulty");
-		gameItemSimple.addActionListener(this);
-		gameItemEasy.addActionListener(this);
-		gameItemDifficulty.addActionListener(this);
-		//startJFrame.setJMenuBar(gameMenuBar);
-		startJFrame.add(gameMenuBar, BorderLayout.NORTH);
-		//gameMenuBar.setBackground(Color.red);
-		gameMenuBar.add(gameMenu);
-		gameMenu.add(gameItemSimple);
-		gameMenu.add(gameItemEasy);
-		gameMenu.add(gameItemDifficulty);
-		
-	}
+        CreatMenuBar();
+        First();
+        startJFrame.setVisible(true);
+    }
 
-	public Component getJpanel(){
-		Component component = null;
-		int count = startJFrame.getComponentCount();
-		for (int i = 0; i < count; i++) {
-			component = startJFrame.getComponent(i);
-			System.out.println(component instanceof JPanel);
-			if (component instanceof JPanel) {
-				JPanel jPanel = (JPanel) component;
-				System.out.println(count + " , " + component);
-				jPanel.remove(jPanel);
-			}
-		}
-		return component;
-	}
-	
-	//@Override
-    public void actionPerformed(ActionEvent e){
-		startJFrame.getContentPane().removeAll();
-		startJFrame.repaint();
-		startJFrame.validate();
-		CreatMenuBar();
-		String difficulty = e.getActionCommand();
+    public void CreatMenuBar() {
+        JMenuBar gameMenuBar = new JMenuBar();
+        JMenu gameMenu = new JMenu("Option");
+        JMenuItem gameItemSimple = new JMenuItem("Simple");
+        JMenuItem gameItemEasy = new JMenuItem("Easy");
+        JMenuItem gameItemDifficulty = new JMenuItem("Difficulty");
+        gameItemSimple.addActionListener(this);
+        gameItemEasy.addActionListener(this);
+        gameItemDifficulty.addActionListener(this);
+        //startJFrame.setJMenuBar(gameMenuBar);
+        startJFrame.add(gameMenuBar, BorderLayout.NORTH);
+        //gameMenuBar.setBackground(Color.red);
+        gameMenuBar.add(gameMenu);
+        gameMenu.add(gameItemSimple);
+        gameMenu.add(gameItemEasy);
+        gameMenu.add(gameItemDifficulty);
 
-		if(difficulty.equals("Simple")){
-			ThunderPanel.tmp = "Simple";
-			startJFrame.setSize(500,300);
-			startJFrame.setLocationRelativeTo(null);
-		}
+    }
 
-		if(difficulty.equals("Easy")){
-			ThunderPanel.tmp = "Easy";
-			startJFrame.setSize(800,600);
-			startJFrame.setLocationRelativeTo(null);
-		}
-		
-		if(difficulty.equals("Difficulty")){
-			ThunderPanel.tmp = "Difficulty";
-			startJFrame.setSize(1280,720);
-			startJFrame.setLocationRelativeTo(null);
-		}
-		startJFrame.add(new ThunderPanel(),BorderLayout.CENTER);
+    public void actionPerformed(ActionEvent e) {
+        startJFrame.getContentPane().removeAll();
+        startJFrame.repaint();
+        startJFrame.validate();
+        lww++;
+        CreatMenuBar();
+        String difficulty = e.getActionCommand();
 
-		startJFrame.setVisible(true);
-	}
-	
-	public static void main(String args[]){
-		new Start();
-	}
+        if (difficulty.equals("Simple")) {
+            ThunderPanel.tmp = "Simple";
+            startJFrame.setSize(500, 300);
+        }
+
+        if (difficulty.equals("Easy")) {
+            ThunderPanel.tmp = "Easy";
+            startJFrame.setSize(800, 600);
+        }
+
+        if (difficulty.equals("Difficulty")) {
+            ThunderPanel.tmp = "Difficulty";
+            startJFrame.setSize(1280, 720);
+        }
+        startJFrame.setLocationRelativeTo(null);
+        startJFrame.add(new ThunderPanel(), BorderLayout.CENTER);
+        startJFrame.setVisible(true);
+    }
+
+    public void First(){
+        if (lww == -1) {
+            JLabel first = new JLabel("请打开Option进行选择", JLabel.CENTER);
+            startJFrame.add(first, BorderLayout.CENTER);
+        }
+    }
+
+    public static void main(String args[]) {
+        new Start();
+    }
 }
--- src/com/kabiuo/ThunderPanel.java
+++ src/com/kabiuo/ThunderPanel.java
@@ -5,226 +5,229 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
-public class ThunderPanel extends JPanel implements MouseListener{
-	private static int row;
-	private static int col;
-	private static int thunderNumber;
-	public static String tmp;
-	int thunderSign = -1;
-	
-	Container container = new Container();
-	JButton border[][];
-	int count[][];
-	
-	public ThunderPanel(){
-		Judge();
+public class ThunderPanel extends JPanel implements MouseListener {
+    private static int row;
+    private static int col;
+    private static int thunderNumber;
+    public static String tmp;
+    int thunderSign = -1;
+    int lw = 0;
 
-		System.out.println();
-		this.removeAll();
-		this.repaint();
-		this.validate();
-		
-		border= new JButton[row][col];
-		count= new int[row][col];
+    Container container = new Container();
+    JButton border[][];
+    int count[][];
 
-		this.setLayout(new BorderLayout());
-		this.add(container, BorderLayout.CENTER);
-		addThunderButtons();
-		addThunder();
-		addCalculatuonThunderNumber();
-	}
+    public ThunderPanel() {
+        Judge();
 
-	public void Judge(){
-		if (tmp.equals("Simple")) {
-			this.row = 9;
-			this.col = 9;
-			this.thunderNumber = 10;
-		}
-		if (tmp.equals("Easy")){
-			this.row = 16;
-			this.col = 16;
-			this.thunderNumber = 40;
-		}
-		if (tmp.equals("Difficulty")){
-			this.row = 16;
-			this.col = 30;
-			this.thunderNumber = 99;
-		}
+//		System.out.println();
+        this.removeAll();
+        this.repaint();
+        this.validate();
 
-	}
-	
-	public void addThunderButtons(){
-		container.setLayout(new GridLayout(row, col));
-		for(int i = 0; i < row; i++){
-			for(int j = 0; j < col; j++){
-				JButton Thunders = new JButton();
-				//System.out.println(i + "," + j);
-				//Thunders.setEnabled(false);
-				Thunders.setOpaque(true);
-				Thunders.addMouseListener(this);
-				border[i][j] = Thunders;
-				container.add(Thunders);
-			}
-		}
-	}
-	
-	public void addThunder(){
-		Random random = new Random();
-		int randomRow, randomCol;
-		for(int i = 0; i < thunderNumber; i++){
-			randomRow = random.nextInt(row);
-			randomCol = random.nextInt(col);
-			
-			//count[randomRow][randomCol] = thunderSign;
-			if(count[randomRow][randomCol] == thunderSign){
-				i--;
-			} else {
-				if(0 == randomRow){
-					if(0 == randomCol){
-						if(count[0][1] == thunderSign && count[1][0] == thunderSign && count[1][1] == thunderSign){
-							i--;
-							continue;
-						}
-					}
-					if(col - 1 == randomCol){
-						if(count[0][col - 2] == thunderSign && count[1][col - 2] == thunderSign && count[1][col - 1] == thunderSign){
-							i--;
-							continue;
-						}
-					}
-				}
-				if(row - 1 == randomRow){
-					if(0 == randomCol){
-						if(count[row - 1][1] == thunderSign && count[row - 2][0] == thunderSign && count[row - 2][1] == thunderSign){
-							i--;
-							continue;
-						}
-					}
-					if(col - 1 == randomCol){
-						if(count[row - 1][col - 2] == thunderSign && count[row - 2][col - 2] == thunderSign && count[row - 2][col - 1] == thunderSign){
-							i--;
-							continue;
-						}
-					}
-				}
-				count[randomRow][randomCol] = thunderSign;
-				//border[randomRow][randomCol].setText("X");
-			}
-		}
-	}
-	
-	public void loseGame(){
-		for(int i = 0; i < row; i++){
-			for(int j = 0; j < col; j++){
-				int counts = count[i][j];
-				if(counts == thunderSign){
-					border[i][j].setText("X");
-					border[i][j].setEnabled(false);
-					border[i][j].setBackground(Color.red);
-				}else{
-					border[i][j].setText(counts+"");
-					border[i][j].setEnabled(false);
-				}
-			}
-		}
-		JOptionPane.showMessageDialog(null, "You Lose!");
-	}
-	
-	public void openCell(int i, int j){
-		if(border[i][j].isEnabled() == false) return;
-		border[i][j].setEnabled(false);
-		if(0 == count[i][j]){
-			if(i > 0 && j > 0 && count[i-1][j-1] != thunderSign) openCell(i-1, j-1);
-			if(i > 0 && count[i-1][j] != thunderSign) openCell(i-1, j);
-			if(i > 0 && j < col - 1 && count[i-1][j+1] != thunderSign) openCell(i-1, j+1);
-			if(j > 0 && count[i][j-1] != thunderSign) openCell(i, j-1);
-			if(j < col - 1 && count[i][j+1] != thunderSign) openCell(i, j+1);
-			if(i < row -1 && j > 0 && count[i+1][j-1] != thunderSign) openCell(i+1, j-1);
-			if(i < row -1 && count[i+1][j] != thunderSign) openCell(i+1, j);
-			if(i < row -1 && j < col - 1 && count[i+1][j+1] != thunderSign) openCell(i+1, j+1);
-		} else {
-			border[i][j].setText(count[i][j] +"");
-		}
-	}
-	
-	public void checkWin(){
-		for(int i = 0; i < row; i++){
-			for(int j = 0; j < col; j++){
-				if(border[i][j].isEnabled() == true && count[i][j] != thunderSign) return;
-			}
-		}
-		
-		JOptionPane.showMessageDialog(null, "You Win!");
-		
-		for(int i = 0; i < row; i++){
-			for(int j = 0; j < col; j++){
-				if(count[i][j] == thunderSign){
-					border[i][j].setText("X");
-					border[i][j].setEnabled(false);
-					border[i][j].setBackground(Color.red);
-				}else{
-					border[i][j].setText(count[i][j]+"");
-					border[i][j].setEnabled(false);
-				}
-			}
-		}
-	}
-	
-	public void addCalculatuonThunderNumber(){
-		int counts;
-		for(int i = 0; i < row; i++){
-			for(int j = 0; j < col; j++){
-				counts = 0;
-				if(count[i][j] == thunderSign) continue;
-				
-				if(i > 0 && j > 0 && count[i-1][j-1] == thunderSign) counts++;
-				if(i > 0 && count[i-1][j] == thunderSign) counts++;
-				if(i > 0 && j < col - 1 && count[i-1][j+1] == thunderSign) counts++;
-				if(j > 0 && count[i][j-1] == thunderSign) counts++;
-				if(j < col - 1 && count[i][j+1] == thunderSign) counts++;
-				if(i < row -1 && j > 0 && count[i+1][j-1] == thunderSign) counts++;
-				if(i < row -1 && count[i+1][j] == thunderSign) counts++;
-				if(i < row -1 && j < col - 1 && count[i+1][j+1] == thunderSign) counts++;
-				
-				count[i][j] = counts;
-				//border[i][j].setText(counts + "");
-			}
-		}
-	}
-	
-	@Override
+        border = new JButton[row][col];
+        count = new int[row][col];
+
+        this.setLayout(new BorderLayout());
+        this.add(container, BorderLayout.CENTER);
+        addThunderButtons();
+        addThunder();
+        addCalculatuonThunderNumber();
+    }
+
+    public void Judge() {
+        if (tmp.equals("Simple")) {
+            this.row = 9;
+            this.col = 9;
+            this.thunderNumber = 10;
+        }
+        if (tmp.equals("Easy")) {
+            this.row = 16;
+            this.col = 16;
+            this.thunderNumber = 40;
+        }
+        if (tmp.equals("Difficulty")) {
+            this.row = 16;
+            this.col = 30;
+            this.thunderNumber = 99;
+        }
+    }
+
+    public void addThunderButtons() {
+        container.setLayout(new GridLayout(row, col));
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                JButton Thunders = new JButton();
+                //System.out.println(i + "," + j);
+                //Thunders.setEnabled(false);
+                Thunders.setOpaque(true);
+                Thunders.addMouseListener(this);
+                border[i][j] = Thunders;
+                container.add(Thunders);
+            }
+        }
+    }
+
+    public void addThunder() {
+        Random random = new Random();
+        int randomRow, randomCol;
+        for (int i = 0; i < thunderNumber; i++) {
+            randomRow = random.nextInt(row);
+            randomCol = random.nextInt(col);
+
+            //count[randomRow][randomCol] = thunderSign;
+            if (count[randomRow][randomCol] == thunderSign) {
+                i--;
+            } else {
+                if (0 == randomRow) {
+                    if (0 == randomCol) {
+                        if (count[0][1] == thunderSign && count[1][0] == thunderSign && count[1][1] == thunderSign) {
+                            i--;
+                            continue;
+                        }
+                    }
+                    if (col - 1 == randomCol) {
+                        if (count[0][col - 2] == thunderSign && count[1][col - 2] == thunderSign && count[1][col - 1] == thunderSign) {
+                            i--;
+                            continue;
+                        }
+                    }
+                }
+                if (row - 1 == randomRow) {
+                    if (0 == randomCol) {
+                        if (count[row - 1][1] == thunderSign && count[row - 2][0] == thunderSign && count[row - 2][1] == thunderSign) {
+                            i--;
+                            continue;
+                        }
+                    }
+                    if (col - 1 == randomCol) {
+                        if (count[row - 1][col - 2] == thunderSign && count[row - 2][col - 2] == thunderSign && count[row - 2][col - 1] == thunderSign) {
+                            i--;
+                            continue;
+                        }
+                    }
+                }
+                count[randomRow][randomCol] = thunderSign;
+                //border[randomRow][randomCol].setText("X");
+            }
+        }
+    }
+
+    public void loseGame() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                int counts = count[i][j];
+                if (counts == thunderSign) {
+                    border[i][j].setText("X");
+                    border[i][j].setEnabled(false);
+                    border[i][j].setBackground(Color.red);
+                } else {
+                    border[i][j].setText(counts + "");
+                    border[i][j].setEnabled(false);
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(null, "You Lose!");
+    }
+
+    public void openCell(int i, int j) {
+        if (border[i][j].isEnabled() == false) return;
+        border[i][j].setEnabled(false);
+        if (0 == count[i][j]) {
+            if (i > 0 && j > 0 && count[i - 1][j - 1] != thunderSign) openCell(i - 1, j - 1);
+            if (i > 0 && count[i - 1][j] != thunderSign) openCell(i - 1, j);
+            if (i > 0 && j < col - 1 && count[i - 1][j + 1] != thunderSign) openCell(i - 1, j + 1);
+            if (j > 0 && count[i][j - 1] != thunderSign) openCell(i, j - 1);
+            if (j < col - 1 && count[i][j + 1] != thunderSign) openCell(i, j + 1);
+            if (i < row - 1 && j > 0 && count[i + 1][j - 1] != thunderSign) openCell(i + 1, j - 1);
+            if (i < row - 1 && count[i + 1][j] != thunderSign) openCell(i + 1, j);
+            if (i < row - 1 && j < col - 1 && count[i + 1][j + 1] != thunderSign) openCell(i + 1, j + 1);
+        } else {
+            border[i][j].setText(count[i][j] + "");
+        }
+    }
+
+    public void checkWin() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (border[i][j].isEnabled() == true && count[i][j] != thunderSign) return;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "You Win!");
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (count[i][j] == thunderSign) {
+                    border[i][j].setText("X");
+                    border[i][j].setEnabled(false);
+                    border[i][j].setBackground(Color.red);
+                } else {
+                    border[i][j].setText(count[i][j] + "");
+                    border[i][j].setEnabled(false);
+                }
+            }
+        }
+    }
+
+    public void addCalculatuonThunderNumber() {
+        int counts;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                counts = 0;
+                if (count[i][j] == thunderSign) continue;
+
+                if (i > 0 && j > 0 && count[i - 1][j - 1] == thunderSign) counts++;
+                if (i > 0 && count[i - 1][j] == thunderSign) counts++;
+                if (i > 0 && j < col - 1 && count[i - 1][j + 1] == thunderSign) counts++;
+                if (j > 0 && count[i][j - 1] == thunderSign) counts++;
+                if (j < col - 1 && count[i][j + 1] == thunderSign) counts++;
+                if (i < row - 1 && j > 0 && count[i + 1][j - 1] == thunderSign) counts++;
+                if (i < row - 1 && count[i + 1][j] == thunderSign) counts++;
+                if (i < row - 1 && j < col - 1 && count[i + 1][j + 1] == thunderSign) counts++;
+
+                count[i][j] = counts;
+                //border[i][j].setText(counts + "");
+            }
+        }
+    }
+
+    @Override
     public void mouseClicked(MouseEvent e) {
-		JButton button = (JButton)e.getSource();
-        //System.out.println("鼠标点击---"+"\t"); 
-		int counts = -99;
-		for(int i = 0; i < row; i++){
-			for(int j = 0; j < col; j++){
-				if(button.equals(border[i][j])){
-					counts = count[i][j];
-					if(counts == thunderSign){
-						loseGame();
-					} else {
-						openCell(i, j);
-						checkWin();
-					}
-				}
-			}
-		}
+        if (0 == lw) {
+            JButton button = (JButton) e.getSource();
+            //System.out.println("鼠标点击---"+"\t");
+            int counts = -99;
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (button.equals(border[i][j])) {
+                        counts = count[i][j];
+                        if (counts == thunderSign) {
+                            lw += 1;
+                            loseGame();
+                        } else {
+                            openCell(i, j);
+                            checkWin();
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {   
+    public void mousePressed(MouseEvent e) {
     }
 
     @Override
-    public void mouseReleased(MouseEvent e) {   
+    public void mouseReleased(MouseEvent e) {
     }
 
     @Override
-    public void mouseEntered(MouseEvent e) { 
+    public void mouseEntered(MouseEvent e) {
     }
 
     @Override
-    public void mouseExited(MouseEvent e) { 
+    public void mouseExited(MouseEvent e) {
     }
 }
